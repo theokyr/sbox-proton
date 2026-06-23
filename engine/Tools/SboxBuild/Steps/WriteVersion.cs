d44e7d33cd2c0b4028f66a53a100382bdfef2855
theokyr <theo.kyrr@gmail.com>
2026-06-23 23:20:23 +0300

    fix: write stable mcp version timestamps

--- engine/Tools/SboxBuild/Steps/WriteVersion.cs
+++ engine/Tools/SboxBuild/Steps/WriteVersion.cs
@@ -1,4 +1,5 @@
-﻿using static Facepunch.Constants;
+﻿using System.Globalization;
+using static Facepunch.Constants;
 
 namespace Facepunch.Steps;
 
@@ -18,7 +19,7 @@ internal class WriteVersion( string name ) : Step( name )
 							  $"{Environment.GetEnvironmentVariable( "GITHUB_RUN_ID" )}\n" +
 							  $"{Environment.GetEnvironmentVariable( "GITHUB_JOB" )}\n" +
 							  $"{Environment.GetEnvironmentVariable( "GITHUB_ACTOR" )}\n" +
-							  $"{DateTime.UtcNow}\n";
+							  $"{DateTime.UtcNow.ToString( "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture )}\n";
 
 			File.WriteAllText( target, contents );
 
